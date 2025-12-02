1398efe35c5b42dbdc3e4a15aa86f569c4f210d9
Ziad Skafi <ziad.skafi12@gmail.com>
2025-12-02 21:12:43 +0400

    first commit

---
+++ docs/scripts/auth.js
@@ -0,0 +1,64 @@
+const API_BASE = "http://192.168.1.179:8080";
+
+async function signup() {
+    const username = document.getElementById("signUpUsername").value.trim();
+    const password = document.getElementById("signUpPassword").value.trim();
+    if (!username || !password) {
+        alert("Please enter username and password");
+        return;
+    }
+
+    const res = await fetch(`${API_BASE}/signup`, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ username, password }),
+    });
+
+    const data = await res.json()
+    if (res.ok) {
+        alert("Signup succesful! You can login now.");
+    } else {
+        alert(data.error || "Signup failed");
+    }
+}
+
+async function loadUser() {
+    if (!token) return;
+
+    const res = await fetch(`${API_BASE}/me`, {
+        headers: { "Authorization": `Bearer ${token}` }
+    });
+
+    if (!res.ok) {
+        // invalid token or expired
+        localStorage.removeItem("token");
+        return;
+    }
+
+    const data = await res.json();
+    localStorage.setItem("username", data.username);
+}
+
+async function login() {
+    const username = document.getElementById("loginUsername").value.trim();
+    const password = document.getElementById("loginPassword").value.trim();
+    if (!username || !password) {
+        alert("Please enter username and password");
+        return;
+    }
+
+    const res = await fetch(`${API_BASE}/login`, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ username, password }),
+    });
+
+    const data = await res.json();
+    if (res.ok && data.token) {
+        localStorage.setItem("token", data.token);
+        loadUser();
+        alert("Login successful!");
+    } else {
+        alert(data.error || "Login failed");
+    }
+}
---
+++ docs/scripts/dashboard.js
@@ -0,0 +1,39 @@
+const token = localStorage.getItem("token");
+const username = localStorage.getItem("username");
+
+const themeToggle = document.getElementById("themeToggle");
+
+// load saved theme
+const savedTheme = localStorage.getItem("theme") || "light";
+document.body.classList.toggle("dark", savedTheme === "dark");
+
+// update button icon
+themeToggle.textContent = savedTheme === "dark" ? "☀️" : "🌙";
+
+// toggle theme
+themeToggle.addEventListener("click", () => {
+    const newTheme = document.body.classList.toggle("dark") ? "dark" : "light";
+    localStorage.setItem("theme", newTheme);
+    themeToggle.textContent = newTheme === "dark" ? "☀️" : "🌙";
+});
+
+function updateButtons() {
+    const token = localStorage.getItem("token");
+
+    document.getElementById("logoutBtn").style.display = token ? "block" : "none";
+    document.getElementById("signUpBtn").style.display = token ? "none" : "block";
+    document.getElementById("loginBtn").style.display = token ? "none" : "block";
+}
+
+document.getElementById("logoutBtn").addEventListener("click", () => {
+    localStorage.removeItem("token");
+    localStorage.removeItem("username");
+    document.getElementById("username").innerHTML = "";
+    updateButtons();
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+    updateButtons();
+    if (!username) return;
+    document.getElementById("username").innerHTML = username;
+});
